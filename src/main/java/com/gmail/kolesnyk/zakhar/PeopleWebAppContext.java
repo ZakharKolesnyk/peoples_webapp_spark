fd5845eb5cf185435ef33fa03f159396556c696d
ZakharKolesnyk <zakhar.kolesnyk@gmail.com>
2018-01-18 14:21:46 +0200

    first

--- src/main/java/com/gmail/kolesnyk/zakhar/PeopleWebAppContext.java
+++ src/main/java/com/gmail/kolesnyk/zakhar/PeopleWebAppContext.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 import static spark.Spark.port;
 
 public class PeopleWebAppContext {
+
     static {
         //spark configuration
         port(8080);
@@ -46,6 +47,5 @@ public class PeopleWebAppContext {
     }
 
 
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) {}
 }
